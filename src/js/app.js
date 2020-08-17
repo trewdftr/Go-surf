6733d4490fc712aa5516f7488e0cce27a6500785
Savauge <kolesnik1310.dev@gmail.com>
2020-08-17 20:15:46 +0300

    Add slider for project

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-$('h1').css('color', 'green');
+$(function(){
+    $('.header__slider').slick({
+        infinite: true,
+        swipe: false,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<img class="header__slider-arrow header__slider-arrow--left" src="./images/dest/arrow-lf.svg" alt="left">',
+        nextArrow: '<img class="header__slider-arrow header__slider-arrow--right" src="./images/dest/arrow-rg.svg" alt="right">',
+    });
+});
