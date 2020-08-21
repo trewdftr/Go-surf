65a92d1ecb922efbde78b754dd80cdfa564d3fbe
Savauge <kolesnik1310.dev@gmail.com>
2020-08-21 18:56:36 +0300

    Fix slider syncing remove scrolling

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
 $(function(){
     $('.header__slider').slick({
         infinite: true,
-        swipe: false,
         fade: true,
-        cssEase: 'linear',
         prevArrow: '<img class="header__slider-arrow header__slider-arrow--left" src="./images/dest/arrow-lf.svg" alt="left">',
         nextArrow: '<img class="header__slider-arrow header__slider-arrow--right" src="./images/dest/arrow-rg.svg" alt="right">',
+        asNavFor: '.slider-dots',
     });
+
+    $('.slider-dots').slick({
+        asNavFor: '.header__slider',
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+
 });
